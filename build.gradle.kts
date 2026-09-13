1029926f7c008d0d60f6e2524ba3aeb63c9a2a26
A Cat <wth110628@163.com>
2026-09-13 17:31:11 +0800

    修改构建配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,31 +92,5 @@ publishing {
 	}
 
 	// See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
-	repositories {
-		maven("https://mvn.devos.one/releases") {
-			name = "devOS"
-			credentials {
-				username = System.getenv()["MAVEN_USER"]
-				password = System.getenv()["MAVEN_PASS"]
-			}
-		}
-	}
-}
-
-tasks.named("publishMavenJavaPublicationToDevOSRepository") {
-	onlyIf {
-		val group = project.property("maven_group") as String
-		val artifactId = project.property("archives_base_name") as String
-		val version = project.version.toString()
-
-		try {
-			val connection = URI.create("https://mvn.devos.one/releases/${group.replace(".", "/")}/${artifactId}/${version}/${artifactId}-${version}.jar").toURL().openConnection() as HttpURLConnection
-			connection.requestMethod = "GET"
-			connection.connect()
-
-			connection.responseCode != 200
-		} catch (_: Exception) {
-			false
-		}
-	}
+	repositories {}
 }
